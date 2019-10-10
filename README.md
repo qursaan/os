91da24298384a780f2b3d9d4eb7f110f0a331002
qursaan <qursaan@gmail.com>
2019-10-11 01:02:08 +0200

    fix: random generator
    Update random generator batch

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 ## Contents
 
-- batch_generator: to generate a big random number for testing purpose.
+- **rn_generator**: Generate a big random number for testing purpose.
+```
+Usage: .\rn_generator <-n NUMBER | -r NUMBER | -f FILENAME>
+Options:
+        -h,--help               Show this help message
+        -n,--numbers NUMBER     the no of number generated (default 1000)
+        -r,--range NUMBER       the range of number generated from 0 (default 10)
+        -f,--filename FILENAME  the name of output file (default bigfile.txt)
+```
---
+++ rn_generator.cpp
@@ -0,0 +1,78 @@
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+
+using namespace std;
+
+static void show_usage(std::string name)
+{
+    std::cerr << "Usage: " << name << "<-n NUMBER | -r NUMBER | -f FILENAME>\n"
+              << "Options:\n"
+              << "\t-h,--help\t\tShow this help message\n"
+              << "\t-n,--numbers NUMBER\tthe no of number generated (default 1000)\n"
+              << "\t-r,--range NUMBER\tthe range of number generated from 0 (default 10)\n"
+              << "\t-f,--filename FILENAME\tthe name of output file (default bigfile.txt)"
+              << std::endl;
+}
+
+int main(int argc , char** argv){
+    // default values
+	int numbers  = 1000;
+	int range    = 10;
+	string fname = "bigfile.txt";
+
+	// READ ARGUMENTS
+	try{
+        for (int i = 1; i < argc; ++i) {
+            std::string arg = argv[i];
+
+            if ((arg == "-h") || (arg == "--help")) {
+                show_usage(argv[0]);
+                return 0;
+            } else if ((arg == "-n") || (arg == "--numbers")) {
+                if (i + 1 < argc) {
+                    numbers = stoi(argv[++i]);
+                } else {
+                    std::cerr << "--numbers option requires one argument." << std::endl;
+                    return 1;
+                }
+            } else if ((arg == "-r") || (arg == "--range")) {
+                if (i + 1 < argc) {
+                    range = stoi(argv[++i]);
+                } else {
+                    std::cerr << "--range option requires one argument." << std::endl;
+                    return 1;
+                }
+            }if ((arg == "-f") || (arg == "--filename")) {
+                if (i + 1 < argc) {
+                    fname = argv[++i];
+                } else {
+                    std::cerr << "--filename option requires one argument." << std::endl;
+                    return 1;
+                }
+            }
+        }
+	} catch(std::exception& e) {
+        show_usage(argv[0]);
+    }
+    cout << "Start generate using N(" << numbers << ") and Range (0-" << range-1 << ") \n";
+	srand( time(0) ); // generate random based on time
+
+	ofstream outputFile;
+	outputFile.open(fname);
+	int rn;
+
+	for(int i=0; i<numbers; i++)
+	{
+		rn = rand()% range + 1;
+		outputFile << rn  << ' ';
+		if(i%16==0) cout << "\b\b"<< std::flush <<": ";
+        else if(i%16==4) cout << "\b\b"<< std::flush <<"..";
+        else if(i%16==8) cout << "\b\b"<< std::flush <<" :";
+        else if(i%16==12) cout << "\b\b"<< std::flush <<"::";
+	}
+	outputFile.close();
+    cout << "\b\bSaved in " << fname << endl;
+	return 0;
+}
