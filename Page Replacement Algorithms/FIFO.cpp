c9ea4fc8c5c73d070d45e28efda68cc73bdc5e66
qursaan <qursaan@gmail.com>
2019-10-11 02:46:33 +0200

    add how to use fifo

--- Page Replacement Algorithms/FIFO.cpp
+++ Page Replacement Algorithms/FIFO.cpp
@@ -1,61 +1,179 @@
 #include <iostream>
-
+#include <fstream>
+#include <string.h>
+#include "helper.h"
 using namespace std;
-void printarray(int *a, int size){
-	for(int  i = 0 ; i< size ; i++)
-	{
-		if(a[i] !=-1){
-			cout << a[i] << "\t";
-		}else{
-			cout << "X\t" ;
-		}
-	}
-	cout << endl;
-}
-int sequenialsearch(int *a, int size, int key){
-	for(int i=0 ; i<size ; i++)
-	{	
-		if(key == a[i])	return i;
-	}
-	return -1;
-}
-int main(int argc, char**argv){
-	int *p; //pointer to store array of memory
-	if(argc<2){
-		cout<<"Please enter memory size\n";
-		return -1;
-	}
-	int memory_size = stoi(argv[1]); 
-	p = new int[memory_size];
-	//initalize array 
-	for(int i=0; i<memory_size;  i++)
-		p[i] = -1;
-	
-	int s = 0;//no of success
-	int f = 0;//no of fault
-	int t_data = argc - 2; //no of data
-	int index = -1;//fifo  
 
-	for(int j=0; j<t_data ; j++) // allocate data in memory
-	{
-		int data = stoi(argv[j+2]);
-		//search to see if data is already in memory
-		int r = sequenialsearch(p,memory_size, data);
-		if(r == -1){ // not found
-			index++; //fifo order
-			if(index>=memory_size) 
-				index=0;
-			p[index] = data;
-			f++;
-		}else{
-			s++;
-		}
-		printarray(p,memory_size);
-	}
-
-	double success_rate =s*1.0 / t_data;
-	cout << "Success rate = " << s << "/" << t_data << "=" << success_rate << endl;
-	cout << "Page Fault = " << f << endl;
-	delete p; 
-	return 0;
+static void show_usage(std::string name)
+{
+    std::cerr << "Usage: " << name << "  <-i FILENAME> [-o FILENAME | -p NUMBER | -a | -s]\n"
+              << "Required:\n"
+              << "\t-i,--input FILENAME\tthe name of input file\n"
+              << "Options:\n"
+              << "\t-h,--help\t\tShow this help message\n"
+              << "\t-o,--output FILENAME\tthe name of output file (default output.csv)\n"
+              << "\t-p,--pagesize NUMBER\tthe max of page generated (default 10)\n"
+              << "\t-a,--analysis\t\tBrute-force all page sizes\n"
+              << "\t-s,--show\t\tPrint steps\n"
+              << std::endl;
 }
+
+int main(int argc,char**argv)
+{
+    if(argc<3)
+    {
+        show_usage(argv[0]);
+        return 0;
+    }
+    string  input_filename = "input.txt";
+    string output_filename = "output.csv";
+    int max_pagesize = 10;
+    bool print = false;
+    bool analysis = false;
+
+
+    /*
+        0. READ ARGUEMENTS
+    */
+    try
+    {
+        for (int i = 1; i < argc; ++i)
+        {
+            std::string arg = argv[i];
+
+            if ((arg == "-h") || (arg == "--help"))
+            {
+                show_usage(argv[0]);
+                return 0;
+            }
+            else if ((arg == "-i") || (arg == "--input"))
+            {
+                if (i + 1 < argc)
+                {
+                    input_filename = argv[++i];
+                }
+                else
+                {
+                    std::cerr << "--input option requires one argument." << std::endl;
+                    return 1;
+                }
+            }
+            else if ((arg == "-o") || (arg == "--output"))
+            {
+                if (i + 1 < argc)
+                {
+                    output_filename = argv[++i];
+                }
+                else
+                {
+                    std::cerr << "--output option requires one argument." << std::endl;
+                    return 1;
+                }
+            }
+            if ((arg == "-p") || (arg == "--pagesize"))
+            {
+                if (i + 1 < argc)
+                {
+                    max_pagesize = stoi(argv[++i]);
+                }
+                else
+                {
+                    std::cerr << "--pagesize option requires one argument." << std::endl;
+                    return 1;
+                }
+            }
+            if ((arg == "-s") || (arg == "--show"))
+            {
+                print = true;
+            }
+            if ((arg == "-a") || (arg == "--analysis"))
+            {
+                analysis = true;
+            }
+        }
+    }
+    catch(std::exception& e)
+    {
+        show_usage(argv[0]);
+    }
+
+    ofstream writer;
+    ifstream reader;
+
+    if(reader.is_open()){
+        cerr<< "Input File not found!\n";
+        return 1;
+    }
+
+    writer.open(output_filename);
+    writer << "Page Size,Fault Rate\n";
+    cout << "Page Size" <<"\tFault Rate(%)"<< endl;
+
+    /* BRUTE-FORCE ALL PAGE SIZE */
+    for(int pagesize = 1; pagesize <= max_pagesize; pagesize++)
+    {
+        if(!analysis)
+        {
+            pagesize = max_pagesize;
+        }
+        /* START READING */
+        reader.open(input_filename);
+
+        /*
+            1. CREATE MEMORY ARRAY
+        */
+        int memory_size = pagesize;
+        int *memory = new int[memory_size];
+        memset(memory,-1,memory_size*sizeof(memory));
+
+        /*
+            2. PAGING
+        */
+        int counter = 0;   /* Count Data   */
+        int fault   = 0;   /* Count Faults */
+        int idx     = -1;  /* FIFO Flag    */
+        int key;           /* Element      */
+
+        /*  2.1 READ REQUESTS  */
+        while(reader>>key)
+        {
+            /*  2.2 LOOKUP IN MEMORY  */
+            int result = sequenialSearch(memory,memory_size,key);
+
+            /*  2.3 NOT FOUND - PAGE IT ON QUEUE */
+            if(result == -1)
+            {
+                fault++;
+                idx=(idx+1)%memory_size;
+                memory[idx] = key;
+            }//IF
+
+            if(print)
+            {
+                printArray(memory,memory_size);
+            }
+
+            ++counter; // READING STEP
+        }//FOR
+        if(counter==0){
+            cerr << "Input file is empty!";
+            return 1;
+        }
+
+        float fault_rate = (fault*100.0)/counter;
+        /*
+            3. WRITE RESULTS
+        */
+        writer << pagesize <<","<<fault_rate<< "%\n";
+        cout   <<"\t" <<pagesize <<"\t"<<fault_rate <<" %"<< endl;
+
+
+        /* FREE UP MEMORY */
+        delete memory;
+        reader.close();
+    }
+    writer.close();
+    cout   << "Saved Output in " << output_filename << "\n";
+    return 0;
+
+}//MAIN
---
+++ Page Replacement Algorithms/README.md
@@ -0,0 +1,16 @@
+# Page Replacement Algorithms
+
+## How to use
+
+- **fifo**: Paging using FIFO
+```
+Usage: .\os.exe <-i FILENAME> [-o FILENAME | -p NUMBER | -a | -s]
+Required:
+        -i,--input FILENAME     the name of input file
+Options:
+        -h,--help               Show this help message
+        -o,--output FILENAME    the name of output file (default output.csv)
+        -p,--pagesize NUMBER    the max of page generated (default 10)
+        -a,--analysis           Brute-force all page sizes
+        -s,--show               Print steps
+```
